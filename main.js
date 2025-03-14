f77e6e11fd74136894b784134bfb17769b559abe
Bridget Orr <bridgetorr1902@gmail.com>
2025-03-14 13:42:51 -0400

    adding main functionality with answer and results display

--- main.js
+++ main.js
@@ -58,42 +58,71 @@ class TriviaGame{
     }
 
     displayChoices(currentQ){
-        //display answers
+
+        //get answers
         const correctAnswer = currentQ.results[0].correct_answer;
         const otherChoices = currentQ.results[0].incorrect_answers;
         
         let choices = [correctAnswer, ...otherChoices]
         let choicesDisplay = choices.slice(0,choices.length);
         
-        let randomIndex
+        let randomIndex = 0;
 
         //display choices to user in a random order
-        for(let i=0; i<choices.length; i++){
+        for(let i=choices.length; i>0; i--){
+
             //get random index of elements left to display
-            randomIndex = currentQ.type === 'boolean' ? Math.random()*2 : Math.random()*4;
-            randomIndex = Math.floor(randomIndex);
-            // console.log(`the random index for a ${4-i} length array was ${randomIndex}`);
-            // console.log(i);
+            randomIndex = Math.floor(Math.random() * i);
+
+            //remove that element from array
+            let removedChoice = choicesDisplay.splice(randomIndex, 1);
+            console.log(`removed ${removedChoice}, remaining choices are ${choicesDisplay}`)
             
-            console.log(choicesDisplay);
-            console.log(choicesDisplay[randomIndex]);
-
             //create the html element
-            // let li = document.createElement('li');
-            // li.textContent = choicesDisplay[randomIndex];
-            // console.log(li.innerHTML);
+            let li = document.createElement('li');
+            li.textContent = removedChoice;
 
-            // document.querySelector('#choices').appendChild(li);
-
-           
-
-            choicesDisplay.splice(randomIndex, 1);
+            document.querySelector('#choices').appendChild(li);
+            this.eventListenerforChoice(li, correctAnswer);
         }
-        
-        //add event listeners to each answer
-            //when clicked, all answer is revealed 
     }
 
+    eventListenerforChoice(li, correctAnswer){
+        li.addEventListener('click', () => {
+            if(li.innerHTML === correctAnswer){
+                li.style.backgroundColor = 'rgb(80, 166, 84)';
+                this.nextQuestion(true, correctAnswer);
+            }
+            else{
+                li.style.backgroundColor = 'rgb(201, 60, 60)';
+                this.nextQuestion(false, correctAnswer);
+            }
+        });
+    }
+
+    nextQuestion(result, correctAnswer){
+
+        let resultMessage = document.createElement('span');
+        if(result){
+            resultMessage.innerHTML = 'That\'s correct!';
+        }
+        else{
+            resultMessage.innerHTML = `Wrong, correct answer was ${correctAnswer}`;
+        }
+
+        let next = document.createElement('button');
+        next.innerHTML = 'Next Question'
+        next.id = 'nextButton'
+
+        document.querySelector('#result').appendChild(resultMessage);
+        document.querySelector('#result').appendChild(next);
+
+        document.querySelector('#nextButton').addEventListener('click', () => {
+            //remove prev answers 
+            document.querySelector('#choices').innerHTML = '';
+            document.querySelector('#result').innerHTML = '';
+            this.makeAPICall();
+        });
+    }
 
 }
-
